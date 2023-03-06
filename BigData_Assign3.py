200b290d4c17bc9ff05690f939623b16504718d9
Forest McDonald <forest383@gmail.com>
2023-03-06 17:34:18 -0500

    "Middle of assignment 4, trying to add all nulls"

--- BigData_Assign3.py
+++ BigData_Assign3.py
@@ -1,14 +1,17 @@
 #Author: Michael Forest McDonald
 #Assignment 3
 #03/02/2023
+#Assignment 4
+#03/02/2023
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 plt.ion()
 
 #read the data file and store it as a data frame
-df = pd.read_csv("F:\CSU\BigData_Assign3_repo\gapminder.tsv", sep = '\t')
+df = pd.read_csv("gapminder.tsv", sep = '\t')
 
 # exploration commmands
 print(df.head(5),'\n', 
@@ -48,7 +51,48 @@ df3 = df.groupby("year")["lifeExp"].mean()
 
 df3.plot()
 
+#######################################################################
+#Assignment 4
+
+df4 = pd.read_csv("concat_1.csv")
+df5 = pd.read_csv("concat_2.csv")
+df6 = pd.read_csv("concat_3.csv")
+
+dfConcat = pd.concat([df4, df5, df6])
+
+print(dfConcat)
+
+dfConcat2 = pd.concat([df4, df5, df6], axis = 1)
+
+print(dfConcat2)
+
+person = pd.read_csv("survey_person.csv")
+site = pd.read_csv("survey_site.csv")
+survey = pd.read_csv("survey_survey.csv")
+visited = pd.read_csv("survey_visited.csv")
+
+merge1 = site.merge(visited, left_on = "name", right_on = "site")
+
+print(merge1)
+
+ebola = pd.read_csv("country_timeseries.csv")
+
+print("This is ebola.count ", '\n', ebola.count(), '\n')
+
+num_rows = ebola.shape[0]
+
+num_missing = num_rows - ebola.count()
+
+print(num_missing)
+
+#def allNullEbola(x):
+#    for i in x:
+#        
+#print("This is allNullEbola: ", '\n', allNullEbola(num_missing), '\n')
 
 
 
 
+print(ebola.isnull())
+
+print(np.count_nonzero(ebola.isnull()))
